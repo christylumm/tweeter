998d4a2a0422fa4dcfaaf214395cb9194f377217
Christy Lum <christy.lum121@gmail.com>
2020-08-11 23:25:49 +0000

    Functionality working for character counter, changes colour when over 140 characters

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  //Selects the textarea for the form inside the .new-tweet section
+
+  $('#tweet-text').on('keyup', function() {
+    const counter = document.querySelector('.counter')
+    const maxChars = 140;
+    const charCount = $('#tweet-text').val().length;
+    const remainingChars = maxChars - Number(charCount);
+    counter.innerHTML = remainingChars;
+
+    console.log(remainingChars);
+
+    //chance colour of character counter
+    if (remainingChars < 0) {
+      console.log('this works');
+      counter.style.color = 'red';
+    } else {
+      counter.style.color = 'black';
+    }
+  });
+});
